57db17c6c21de39aac199fce6026b711ee78a224
Brandon Baker <bbaker.sh@gmail.com>
2022-06-26 17:01:43 -0700

    added logic for player input

--- RPS.JS/RPS.js
+++ RPS.JS/RPS.js
@@ -1,7 +1,20 @@
 function computerPlay(){
-    let arr = ["ROCK", "PAPER", "SCISSPRS"];
+    let arr = ["ROCK", "PAPER", "SCISSORS"];
     let picker = Math.floor(Math.random()*(3))
-    console.log(arr[picker])
+    return arr[picker]
 }
 
+function humanPlay(){
+    let playerInput = prompt("Rock Paper or Scissors")
+    let input = playerInput.toUpperCase()
+    if (input === "ROCK"){
+        console.log("ROCK is players pick")
+    } else if (input === "PAPER") {
+        console.log("PAPER is players pick")
+    } else if (input === "SCISSORS") {
+        console.log("SCISSORS is players pick")
+    } else {
+        console.log('Please choose: Rock, Paper, or Scissors!')
+    }
+}
 
